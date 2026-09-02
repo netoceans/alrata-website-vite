9e01c4c61ba0e8c6cc8633f4f7d38524b9abe66f
Omar Suleeq <sleeqwebdesign@gmail.com>
2026-09-03 01:31:11 +0400

    fix: ensure base path is set correctly in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
+    base: '/alrata-website-vite/',
     plugins: [react(), tailwindcss()],
 });
